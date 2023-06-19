d4bd156007adbef74914547da4d3fbf59772ad3b
Rick van 't Hof <rick@ridecheck.app>
2023-06-19 21:34:03 +0200

    add flowlayout for list

--- app/src/main/kotlin/nl/arieck/blockpages/ui/features/character/CharacterScreen.kt
+++ app/src/main/kotlin/nl/arieck/blockpages/ui/features/character/CharacterScreen.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +31,7 @@ import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
+import com.google.accompanist.flowlayout.MainAxisAlignment
 import kotlinx.coroutines.flow.flowOf
 import nl.arieck.blockpages.domain.common.Failure
 import nl.arieck.blockpages.domain.features.character.models.Character
@@ -80,9 +78,9 @@ fun CharacterUi(
 ) {
     Box(
         modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
             .fillMaxSize()
             .navigationBarsPadding()
-            .background(MaterialTheme.colorScheme.background)
     ) {
         LazyColumn(modifier = Modifier.statusBarsPadding(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
             listItems?.let { articlePage ->
@@ -101,52 +99,52 @@ fun CharacterUi(
                             Text(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .background(MaterialTheme.colorScheme.background.copy(0.95f))
+                                    .background(MaterialTheme.colorScheme.secondaryContainer.copy(0.85f))
                                     .padding(start = 24.dp, top = 8.dp, bottom = 8.dp, end = 24.dp),
                                 style = MaterialTheme.typography.labelSmall.copy(color = Color.Gray),
                                 text = "${(idGroup * 10) + 1}-${((idGroup + 1) * 10)}"
                             )
                         }
 
-                        items(charactersInIdGroup) { character ->
-                            articlePage[articlePage.itemSnapshotList.indexOf(character)]
 
-                            Column(
-                                modifier = Modifier
-                                    .clickable {
-                                        onCharacterClick(character.id)
-                                    }
-                                    .background(MaterialTheme.colorScheme.secondaryContainer)
-                                    .padding(16.dp)
+                        item {
+
+                            com.google.accompanist.flowlayout.FlowRow(
+                                modifier = Modifier.fillMaxWidth(),
+                                mainAxisAlignment = MainAxisAlignment.SpaceEvenly,
+                                mainAxisSpacing = 8.dp,
+                                crossAxisSpacing = 8.dp
                             ) {
-                                Text(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    style = MaterialTheme.typography.titleLarge,
-                                    text = character.name.orEmpty(),
-                                    color = MaterialTheme.colorScheme.onSecondaryContainer
-                                )
 
-                                Box {
+                                charactersInIdGroup.forEach { character ->
+                                    articlePage[articlePage.itemSnapshotList.indexOf(character)]
 
-                                    character.imageUrl?.let {
-                                        AsyncImage(
-                                            model = it,
+                                    Box(modifier = Modifier
+                                        .clickable {
+                                            onCharacterClick(character.id)
+                                        }
+                                        .background(MaterialTheme.colorScheme.primaryContainer)
+                                        .padding(8.dp)) {
+
+                                        character.imageUrl?.let {
+                                            AsyncImage(
+                                                model = it,
+                                                modifier = Modifier
+                                                    .padding(16.dp),
+                                                contentDescription = null
+                                            )
+                                        }
+                                        Text(
                                             modifier = Modifier
-                                                .padding(16.dp)
-                                                .fillMaxWidth(),
-                                            contentScale = ContentScale.FillWidth,
-                                            contentDescription = null
+                                                .align(Alignment.BottomEnd)
+                                                .background(MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.5f))
+                                                .padding(16.dp),
+                                            text = character.id,
+                                            style = MaterialTheme.typography.labelLarge.copy(fontSize = 30.sp),
+                                            textAlign = TextAlign.End
                                         )
                                     }
-                                    Text(
-                                        modifier = Modifier
-                                            .align(Alignment.BottomEnd)
-                                            .background(MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.5f))
-                                            .padding(16.dp),
-                                        text = character.id,
-                                        style = MaterialTheme.typography.labelLarge.copy(fontSize = 30.sp),
-                                        textAlign = TextAlign.End
-                                    )
+
                                 }
                             }
                         }
